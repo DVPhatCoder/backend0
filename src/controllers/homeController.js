ac17241930ecef20e7e596851e71aa0c5c6958c5
PhatDv <phatdv@human-life.vn>
2025-07-29 10:44:19 +0700

    create navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,7 @@
 const connection = require("../config/database")
 
 const getHomePage = (req, res) => {
-  //simple connection
-connection.query(
-  'SELECT * from Users u',
-  function (err, results) {
-    console.log(">>results: ",results); // results contains rows returned by server
-    res.send(JSON.stringify(results))
-  }
-);
-  
+ return res.render("homePage.ejs");
 };
 const getSample = (req, res) => {
   res.render("sample.ejs");
