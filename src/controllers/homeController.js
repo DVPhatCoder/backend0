6f0d5cb2acfe5ce746c6d1ade097718a8ec65639
PhatDv <phatdv@human-life.vn>
2025-07-29 10:22:40 +0700

    change create connection to create pool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
+const connection = require("../config/database")
+
 const getHomePage = (req, res) => {
-  res.send("sample");
+  //simple connection
+connection.query(
+  'SELECT * from Users u',
+  function (err, results) {
+    console.log(">>results: ",results); // results contains rows returned by server
+    res.send(JSON.stringify(results))
+  }
+);
+  
 };
 const getSample = (req, res) => {
   res.render("sample.ejs");
